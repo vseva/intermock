c2cb11273346c9c94ac83adf2c7f250b8a4e5cde
Vsevolod Denisov <denisov.v.vladi@sberbank.ru>
2021-02-11 11:33:28 +0300

    type fixes

--- src/lib/custom-primitives.ts
+++ src/lib/custom-primitives.ts
@@ -8,7 +8,6 @@ interface InterfaceProps {
 
 const fake = (prop: string) => faker.fake(`{{${prop}}}`);
 
-const generateId = () => fake('random.uuid');
 const generateNumberInRange = (minimum: number, maximum: number) => {
     const min = Math.ceil(minimum);
     const max = Math.floor(maximum);
@@ -17,35 +16,20 @@ const generateNumberInRange = (minimum: number, maximum: number) => {
 };
 const generateGrade = () => generateNumberInRange(1, 15).toString();
 const generateSfId = () => generateNumberInRange(1000000, 9999999).toString();
-const generateNumberUnderTen = () => generateNumberInRange(1, 10).toString();
 const generateNumberUnderHundred = () => generateNumberInRange(1, 100).toString();
 const generatePastDate = () => fake('date.past');
+const generateFutureDate = () => fake('date.future');
 const getRandomItem = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
 const getJobTitle = () => getRandomItem(jobTitles);
 
 export const customPrimitives: {[index: string]: InterfaceProps} = {
-    IFileDTO: {
-        // fileType: () => fake('system.fileType'),
-        // fileName: () => fake('system.fileName'),
-    },
-    IPermissionActionResponse: {
-        entityId: generateId,
-    },
     IJobRequisitionGroupListDtoRequisitions: {
         sfId: generateSfId,
         gradeFrom: generateGrade,
         gradeTo: generateGrade,
-        updated: generatePastDate,
         title: getJobTitle,
     },
-    IJobRequisitionGroupListDtoRequisitionsMembersDelegate: {
-        //id: generateId,
-    },
-    IJobRequisitionGroupListDtoRequisitionsMembersOwner: {
-        //id: generateId,
-    },
     IJobRequisitionGroupListDTO: {
-        groupId: generateId,
         groupName: () => getRandomItem([
             'Закрытые',
             'Открытые',
@@ -54,13 +38,7 @@ export const customPrimitives: {[index: string]: InterfaceProps} = {
     },
     IJobRequisitionPostingResponseWithStateDTO: {
         activeFrom: generatePastDate,
-        activeTo: generatePastDate,
-    },
-    IDeputyItemDTO: {
-        //personId: generateId,
-    },
-    IConditionsOfEmploymentDTO: {
-        firstWorkingDate: generatePastDate,
+        activeTo: generateFutureDate,
     },
     IJobRequisitionPlainWithDepartmentsDto: {
         title: getJobTitle,
@@ -140,5 +118,11 @@ export const customPrimitives: {[index: string]: InterfaceProps} = {
     IPositionDto: {
         gradeLevel: generateGrade,
         positionName: getJobTitle,
+    },
+    ICandidateConsentDtoConsents: {
+        purpose: () => 'CANDIDATE_POOL',
+    },
+    ICandidateApplicationViewDtoPostingInfo: {
+        jobRequisitionDescriptionTitle: getJobTitle,
     },
 };
